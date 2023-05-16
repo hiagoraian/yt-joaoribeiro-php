4a268c1f83c1a9d6211c358e11517766d7232f54
hiagoraian <hiagogoncalves@proton.me>
2023-05-15 22:27:51 -0300

    Método static

--- pages/x6.php
+++ pages/x6.php
@@ -112,6 +112,81 @@
         <p>-> Use o modificador protected quando você desejar restringir o acesso de uma propriedade ou método à própria classe e às suas classes derivadas (subclasses). Isso permite que as classes derivadas herdem e acessem esses elementos, enquanto ainda mantêm a restrição para partes externas. Esse tipo de acesso é útil quando você deseja fornecer uma interface controlada para as classes derivadas, permitindo que elas acessem e modifiquem certos membros internos.</p>
         <p>-> Use o modificador private quando você desejar restringir o acesso de uma propriedade ou método somente à própria classe. Isso significa que esses elementos não podem ser acessados de fora da classe nem por classes derivadas. O uso de propriedades e métodos privados é útil quando você precisa encapsular certos aspectos da implementação da classe e evitar a dependência direta de outras partes do código. Isso promove a segurança e a integridade interna da classe.</p>
     </div>
+    <div class="container-block">
+            <h3>Getters e Setters</h3>
+            <p>São métodos usados para acessar (get) e modificar (set) os valores de propriedades privadas ou protegidas de uma classe. Eles são uma prática comum na programação orientada a objetos para garantir um encapsulamento adequado e fornecer um controle mais preciso sobre o acesso dos dados.</p>
+            <p>Dica: Sempre é bom verificar o tipo de valor que está sendo inserido, exemplo: Fazer um set para idade, e o usuário colocar um nome.</p>
 
+            <?php 
+                class ExemploGet {
+                    private $propriedade = 'Hiago';
+
+                    public function getPropriedades(){
+                        return $this->propriedade;
+                    }
+                }
+
+                $objetoGet = new ExemploGet();
+                # echo $objetoGet->propriedade; Privado.
+                echo $objetoGet->getPropriedades();
+                echo "</br>";
+            ?>
+            <?php 
+                class ExemploSet{
+                    private $propriedade;
+                    
+                    public function setPropriedade($valor){
+                        $this->propriedade = $valor;
+                    }
+                    public function getPropriedade(){
+                        return $this->propriedade;
+                    }
+                }
+                $objetoSet = new ExemploSet();
+                # $objetoSet->propriedade = 'Hiago'; Privado.
+
+                $objetoSet->setPropriedade("Karlla");
+                echo $objetoSet->getPropriedade();
+                echo "</br>";
+            ?>
+    </div>
+    <div class="container-block">
+        <h3>Static</h3>
+        <p>Quando uma propriedade ou método é declarado como estático, ele pertence à classe em si, e não a uma instância especifica da classe. Isso significa que voc~e pode acessar diretamente propriedades estáticos e chamar métodos estáticos sem a necessidade de criar um objeto da classe.</p>
+        
+        <?php 
+            class ExemploStatic{
+                static $propriedade = "Valor propriedade";
+            }
+
+            echo ExemploStatic::$propriedade;
+            echo "</br>";
+        ?>
+        <p>Exemplo prático do uso de static.</p>
+
+        <?php 
+            class Operadores{
+                static function numeroAleatorio($min, $max){
+                    return rand($min, $max);
+                }
+
+                static function calcularFormula($a,$b){
+                    return ($a*$b)+ ($b*$a);
+                }
+                static function criarNome(){
+                    $nomes = ['João', 'Maria', 'Marcos','Mariana', 'Neuza', 'Isabela'];
+                    $apelido = ['Silva','Pereira','Almeida','Oliveira'];
+
+                    return $nomes[rand(0,count($nomes)-1)] . ' ' . $apelido[rand(0,count($apelido)-1)];
+                }
+            }
+
+            echo Operadores::numeroAleatorio(1,20);
+            echo "</br>";
+            echo Operadores::calcularFormula(5,10);
+            echo "</br>";
+            echo Operadores::criarNome();
+            ?>
+    </div>
 </body>
 </html>
